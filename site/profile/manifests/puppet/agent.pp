48ecab4b01440596b4de0f742fa622094ab9bc8d
Josh Beard <beard@puppetlabs.com>
2014-09-16 09:47:57 -0400

    Add comment to agent profile

--- site/profile/manifests/puppet/agent.pp
+++ site/profile/manifests/puppet/agent.pp
@@ -2,6 +2,7 @@ class profile::puppet::agent {
 
   include profile::params
 
+  ## Use the pe_server module to manage some common Puppet settings
   class { 'pe_server':
     is_master                    => false,
     ca_server                    => $profile::params::pe_puppetca_fqdn,
@@ -9,6 +10,12 @@ class profile::puppet::agent {
     filebucket_server            => $profile::params::pe_puppetmaster_fqdn,
     export_puppetdb_whitelist    => false,
     export_console_authorization => false,
+  }
+
+  ## Ensure the "server" points to our generic CNAME
+  augeas { 'puppet.conf_server':
+    context => '/files/etc/puppetlabs/puppet/puppet.conf',
+    changes => "set main/server ${profiles::params::pe_puppetmaster_fqdn}",
   }
 
 }
